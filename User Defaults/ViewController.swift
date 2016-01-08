ce95237eb44bc9e4937c708fd466a06bf60535b0
AKBarcenas <akbarcen@ucsd.edu>
2016-01-08 01:04:20 -0800

    Stored values into defaults

--- User Defaults/ViewController.swift
+++ User Defaults/ViewController.swift
@@ -9,10 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let defaults = NSUserDefaults.standardUserDefaults()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let defaults = NSUserDefaults.standardUserDefaults()
+        defaults.setInteger(25, forKey: "Age")
+        defaults.setBool(true, forKey: "UseTouchID")
+        defaults.setDouble(M_PI, forKey: "Pi")
+        
+        defaults.setObject("Paul Hudson", forKey: "Name")
+        defaults.setObject(NSDate(), forKey: "LastRun")
+        
+        let array = ["Hello", "World"]
+        defaults.setObject(array, forKey: "SavedArray")
+        
+        let dict = ["Name": "Paul", "Country": "UK"]
+        defaults.setObject(dict, forKey: "SavedDict")
     }
 
     override func didReceiveMemoryWarning() {
